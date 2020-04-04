70769f78e209ed7780eefffbd4f67d8b620281de
Alexandr10m <serbackij@gmail.com>
2020-04-04 17:15:22 +0300

    Разбил код на модули

---
+++ src/components/events-container.js
@@ -0,0 +1,11 @@
+const getEvenstsContainer = () => {
+  return `<ul class="trip-days">
+    <li class="trip-days__item  day">
+      <div class="day__info"></div>
+      <ul class="trip-events__list">
+      </ul>
+    </li>
+  </ul>`;
+};
+
+export {getEvenstsContainer};
