363eafc8cac68eaeb0dd7f2d1b4596314220e40e
Alexandr10m <serbackij@gmail.com>
2020-04-14 19:15:37 +0300

    Добавил файл с константными значениями

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+const TYPE_OF_TRIP_POINT = [
+  `Taxi`,
+  `Bus`,
+  `Train`,
+  `Ship`,
+  `Transport`,
+  `Drive`,
+  `Flight`,
+  `Check-in`,
+  `Sightseeing`,
+  `Restaurant`
+];
+
+const MONTH_NAMES = [
+  `JAN`,
+  `FEB`,
+  `MAR`,
+  `APR`,
+  `MAY`,
+  `JUN`,
+  `JUL`,
+  `AUG`,
+  `SEP`,
+  `OCT`,
+  `NOV`,
+  `DEC`,
+];
+export {TYPE_OF_TRIP_POINT, MONTH_NAMES};
