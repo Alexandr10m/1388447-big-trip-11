98debd187e0e6e01090781bfe2c6f45a55e5f472
Alexandr10m <serbackij@gmail.com>
2020-04-21 16:09:03 +0300

    Отредактировал main.js для работы с компонентами

--- src/main.js
+++ src/main.js
@@ -1,43 +1,45 @@
-import {getInfoRouteTmpl} from './components/trip-info';
-import {getPriceTmpl} from './components/price';
-import {getMenuTmpl} from './components/menu';
-import {getFiltersTmpl} from './components/filters';
-import {getSortTmpl} from './components/sorts';
-import {getFormEditorTmpl} from './components/form-editor';
-import {getEvenstsforAllDaysTmpl} from './components/events';
-import {getSectionTmpl} from './components/section';
-import {render, groupingEventsInOrderForDays} from './utils';
+import TripInfoSectionComponent from './components/section';
+import TripInfonComponent from './components/trip-info';
+import PriceComponent from './components/price';
+import MenuComponent from './components/menu';
+import FiltersComponent from './components/filters';
+import EventEditComponenet from './components/form-editor';
+import SortsComponent from './components/sorts';
+import EventsComponent from './components/events';
+import {render, RenderPosition, groupingEventsInOrderForDays} from './utils';
 import {generateEvents} from './mock/event';
 
 const EVENT_COUNT = 20;
-const events = generateEvents(EVENT_COUNT);
 
 const renderTripInfo = () => {
-  render(tripMainElement, getSectionTmpl(), `afterbegin`);
-  const tripInfo = tripMainElement.querySelector(`.trip-main__trip-info`);
-  render(tripInfo, getInfoRouteTmpl());
-  render(tripInfo, getPriceTmpl());
+  const tripInfoContainer = new TripInfoSectionComponent();
+  render(tripMainElement, tripInfoContainer.getElement(), RenderPosition.AFTERBEGIN);
+  render(tripInfoContainer.getElement(), new TripInfonComponent().getElement());
+  render(tripInfoContainer.getElement(), new PriceComponent().getElement());
 };
 const renderControls = () => {
   const tripControlsElemnt = tripMainElement.querySelector(`.trip-main__trip-controls`);
-  render(tripControlsElemnt.children[0], getMenuTmpl(), `afterend`);
-  render(tripControlsElemnt, getFiltersTmpl());
-};
-const renderSorts = () => {
-  render(tripEventsElement, getSortTmpl());
+  render(tripControlsElemnt.children[0], new MenuComponent().getElement(), RenderPosition.AFTEREND);
+  render(tripControlsElemnt, new FiltersComponent().getElement());
 };
 const renderFormEdit = () => {
-  render(tripEventsElement, getFormEditorTmpl(events[0]));
+  render(tripEventsElement, new EventEditComponenet(events[0]).getElement());
+};
+const renderSorts = () => {
+  render(tripEventsElement, new SortsComponent().getElement());
 };
 const renderEvents = (allEvents) => {
-  render(tripEventsElement, getEvenstsforAllDaysTmpl(groupingEventsInOrderForDays(allEvents)));
+  render(tripEventsElement, new EventsComponent(groupingEventsInOrderForDays(allEvents)).getElement());
 };
 
+const events = generateEvents(EVENT_COUNT);
 const tripMainElement = document.querySelector(`.trip-main`);
+
 renderTripInfo();
 renderControls();
 
 const tripEventsElement = document.querySelector(`.trip-events`);
+
 events.sort((prevEvent, nextEvent) => prevEvent.timeFrame.start.getTime() - nextEvent.timeFrame.start.getTime());
 renderFormEdit();
 renderSorts();
