e95d4502e2a063652ecc59f885855323444abb79
Alexandr10m <serbackij@gmail.com>
2020-04-21 17:34:33 +0300

    Создал отдельный компонент события

---
+++ src/components/event.js
@@ -0,0 +1,77 @@
+import {createElement, formatTime, formatDiffenceTime} from '../utils';
+
+const createOfferTmpl = ({title, price}) => {
+  return (
+    `<li class="event__offer">
+      <span class="event__offer-title">${title}</span>
+      &plus;
+      &euro;&nbsp;<span class="event__offer-price">${price}</span>
+    </li>`
+  );
+};
+
+const getEventTmpl = ({typeOfPoint, city, price, offers, timeFrame}) => {
+
+  const isActiveEvent = typeOfPoint === `Sightseeing` || typeOfPoint === `Check-in` || typeOfPoint === `Restaurant`;
+
+  const startTimeOfEvent = formatTime(timeFrame.start);
+  const finishTimeOfEvent = formatTime(timeFrame.finish);
+  const durationTime = formatDiffenceTime(timeFrame);
+
+  const isOffersShowing = !!offers.length;
+  const offerstMarkup = offers.map((offer) => createOfferTmpl(offer)).join(``);
+
+  return `<li class="trip-events__item">
+    <div class="event">
+      <div class="event__type">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${typeOfPoint}.png" alt="Event type icon">
+      </div>
+      <h3 class="event__title">${typeOfPoint} ${isActiveEvent ? `in` : `to`} ${city}</h3>
+
+      <div class="event__schedule">
+        <p class="event__time">
+          <time class="event__start-time" datetime="2019-03-18T10:30">${startTimeOfEvent}</time>
+          &mdash;
+          <time class="event__end-time" datetime="2019-03-18T11:00">${finishTimeOfEvent}</time>
+        </p>
+        <p class="event__duration">${durationTime}</p>
+      </div>
+
+      <p class="event__price">
+        &euro;&nbsp;<span class="event__price-value">${price}</span>
+      </p>
+
+      <h4 class="visually-hidden">Offers:</h4>
+      ${isOffersShowing ?
+    `<ul class="event__selected-offers">
+        ${offerstMarkup}
+      </ul>` : ``}
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
+    </div>
+    </li>`;
+};
+
+export default class Event {
+  constructor(dataEvent) {
+    this._dataEvent = dataEvent;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return getEventTmpl(this._dataEvent);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
