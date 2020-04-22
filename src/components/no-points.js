963db17f6a794b81bb079b124d375c6c07ebd0e6
Alexandr10m <serbackij@gmail.com>
2020-04-22 17:34:24 +0300

    Добавил компонент No-events

---
+++ src/components/no-points.js
@@ -0,0 +1,31 @@
+import {createElement} from '../utils';
+
+const createNoPointsTmpl = () => {
+  return (
+    `<p class="trip-events__msg">
+      Click New Event to create your first point
+    </p>`
+  );
+};
+
+export default class NoPoints {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNoPointsTmpl();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
