a49684fe21b88562d5818d5884ba323628f25e7b
Alexandr10m <serbackij@gmail.com>
2020-04-26 15:27:05 +0300

    Убрал дублирование разметки в component/sort.js

--- src/components/sorts.js
+++ src/components/sorts.js
@@ -1,4 +1,5 @@
 import AbstractComponent from "./abstract-componenet.js";
+import {SORT_OPTION} from "../constants.js";
 
 const SortType = {
   EVENT: `data-event`,
@@ -6,41 +7,27 @@ const SortType = {
   PRICE: `data-price`
 };
 
-// не понимаю почему не работает input radio (checked)
-// Как мне орисовывать разметку сначала с columnName, а когда сортировка, то без него
+const createSortOptionMarkup = (name, isChecked) => {
+  return (
+    `<div class="trip-sort__item  trip-sort__item--${name.toLowerCase()}">
+      <input id="sort-${name.toLowerCase()}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${name.toLowerCase()}" ${isChecked ? `checked=""` : ``}>
+      <label data-sort-type="${SortType[name.toUpperCase()]}" class="trip-sort__btn" for="sort-${name.toLowerCase()}">${name}</label>
+    </div>`
+  );
+};
+
 const createSortTmpl = () => {
-  const columnName = `Day`;
+  const sortOptionMarkup = SORT_OPTION.map((option, index) => createSortOptionMarkup(option, index === 0)).join(``);
+
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-  <span class="trip-sort__item  trip-sort__item--day">${columnName}</span>
+  <span class="trip-sort__item  trip-sort__item--day">Day</span>
 
-  <div class="trip-sort__item  trip-sort__item--event">
-    <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked="">
-    <label data-sort-type="${SortType.EVENT}" class="trip-sort__btn" for="sort-event">Event</label>
-  </div>
-
-  <div class="trip-sort__item  trip-sort__item--time">
-    <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-    <label data-sort-type="${SortType.TIME}" class="trip-sort__btn" for="sort-time">
-      Time
-      <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-        <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"></path>
-      </svg>
-    </label>
-  </div>
-
-  <div class="trip-sort__item  trip-sort__item--price">
-    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-    <label data-sort-type="${SortType.PRICE}" class="trip-sort__btn" for="sort-price">
-      Price
-      <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-        <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"></path>
-      </svg>
-    </label>
-  </div>
+  ${sortOptionMarkup}
 
   <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
 </form>`;
 };
+
 
 export default class Sorts extends AbstractComponent {
   constructor() {
@@ -57,7 +44,6 @@ export default class Sorts extends AbstractComponent {
 
   setSortTypeChangeHandler(handler) {
     this.getElement().addEventListener(`click`, (evt) => {
-      evt.preventDefault();
 
       if (evt.target.tagName !== `LABEL`) {
         return;
