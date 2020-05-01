02e350aa22a1b5a30b5c574fe096cf0a1e299149
Alexandr10m <serbackij@gmail.com>
2020-05-01 16:05:09 +0300

    Добавил PointController и перенес в него отрисовку мероприятия и редактор с обработчиками

---
+++ src/controllers/point.js
@@ -0,0 +1,47 @@
+import EventComponent from "../components/event.js";
+import EventEditorComponent from "../components/form-editor.js";
+import {render, replace} from "../utils/render.js";
+
+
+export default class PointController {
+  constructor(container) {
+    this._container = container;
+    this._eventComponent = null;
+    this._eventEditorComponent = null;
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+  }
+
+  render(event) {
+    this._eventComponent = new EventComponent(event);
+    this._eventEditorComponent = new EventEditorComponent(event);
+
+    this._eventComponent.setEditButtonClickHandler(() => {
+      this._replaceEventToEditor();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._eventEditorComponent.setFormSubmitHandler((evt) => {
+      evt.preventDefault();
+      this._replaceEditorToEvent();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    render(this._container, this._eventComponent);
+  }
+
+  _replaceEventToEditor() {
+    replace(this._eventEditorComponent, this._eventComponent);
+  }
+
+  _replaceEditorToEvent() {
+    replace(this._eventComponent, this._eventEditorComponent);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+    if (isEscKey) {
+      this._replaceEditorToEvent();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
+}
