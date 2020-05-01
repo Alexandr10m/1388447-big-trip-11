9850e98219b300803e2815485f1f42af2ee257c1
Alexandr10m <serbackij@gmail.com>
2020-05-01 19:36:50 +0300

    Добавил свойство onDataChange в  pointController

--- src/controllers/point.js
+++ src/controllers/point.js
@@ -4,8 +4,9 @@ import {render, replace} from "../utils/render.js";
 
 
 export default class PointController {
-  constructor(container) {
+  constructor(container, onDataChange) {
     this._container = container;
+    this._onDataChange = onDataChange;
     this._eventComponent = null;
     this._eventEditorComponent = null;
     this._onEscKeyDown = this._onEscKeyDown.bind(this);
@@ -24,6 +25,10 @@ export default class PointController {
       evt.preventDefault();
       this._replaceEditorToEvent();
       document.removeEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._eventEditorComponent.setFavouritesButtonClickHandler(() => {
+      this._onDataChange(event, Object.assign({}, event, {isFavourite: !event.isFavourite}));
     });
 
     render(this._container, this._eventComponent);
