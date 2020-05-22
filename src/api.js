aaea11df6199865333873cd98a4ebc743298a986
Alexandr10m <serbackij@gmail.com>
2020-05-22 19:33:33 +0300

    Добавил класс API для запросов на сервер

---
+++ src/api.js
@@ -0,0 +1,39 @@
+import PointModel from "./models/point.js";
+
+const Method = {
+  GET: `GET`,
+  POST: `POST`,
+  PUT: `PUT`,
+  DELETE: `DELETE`
+};
+
+const checkStatus = (response) => {
+  if (response.ok) {
+    return response;
+  } else {
+    throw new Error();
+  }
+};
+
+export default class API {
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getEvents() {
+    return this._load({url: `points`})
+      .then((response) => response.json())
+      .then(PointModel.parseEvents);
+  }
+
+  _load({url, method = Method.GET, body = null, headers = new Headers()}) {
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`${this._endPoint}/${url}`, {method, body, headers})
+      .then(checkStatus)
+      .catch((err) => {
+        throw err;
+      });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
+import API from "./api.js";
 import StatsComponent from "./components/stats.js";
 import {MenuItem} from "./components/menu.js";
-import {generateEvents} from "./mock/event.js";
 import Points from "./models/points";
 import TripEventsController from "./controllers/trip-events.js";
 import TripInfoController from "./controllers/trip-info.js";
 import {render} from "./utils/render.js";
 
-const EVENT_COUNT = 20;
+const AUTHORIZATION = `Basic ret3443he4`;
+const END_POINT = `https://11.ecmascript.pages.academy/big-trip/`;
 
-const events = generateEvents(EVENT_COUNT);
-
+const api = new API(END_POINT, AUTHORIZATION);
 const eventsModel = new Points();
-eventsModel.setEvents(events);
 
 const tripMainElement = document.querySelector(`.trip-main`);
 const bodyContainerElement = document.querySelector(`main .page-body__container`);
@@ -20,8 +19,6 @@ const tripEventsElement = document.querySelector(`.trip-events`);
 const tripInfoController = new TripInfoController(tripMainElement, eventsModel);
 const tripEventsController = new TripEventsController(tripEventsElement, eventsModel);
 const statsComponent = new StatsComponent(eventsModel);
-render(bodyContainerElement, statsComponent);
-statsComponent.hide();
 
 tripEventsController.setAddedNewEventHandler(tripInfoController.enableButtonNewEvent);
 tripInfoController.setClickNewEventButtonHandler(tripEventsController.createEvent);
@@ -39,6 +36,10 @@ tripInfoController.setOnClickMenuItem((menuItem) => {
   }
 });
 
-tripInfoController.render();
-tripEventsController.render();
-
+api.getEvents()
+  .then((events) => {
+    eventsModel.setEvents(events);
+    render(bodyContainerElement, statsComponent);
+    statsComponent.hide();
+    tripEventsController.render();
+  });
