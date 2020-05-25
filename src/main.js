11c6d9e6cb97f07cc8d2efaf7bad104ad07ca9ed
Alexandr10m <serbackij@gmail.com>
2020-05-25 19:26:29 +0300

    В точке входа создал запросы и отредактировал на работу с api

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import API from "./api.js";
+import Destinations from "./models/destinations.js";
+import Offers from "./models/offers.js";
 import StatsComponent from "./components/stats.js";
 import {MenuItem} from "./components/menu.js";
 import Points from "./models/points";
@@ -10,14 +12,17 @@ const AUTHORIZATION = `Basic ret3443he4`;
 const END_POINT = `https://11.ecmascript.pages.academy/big-trip/`;
 
 const api = new API(END_POINT, AUTHORIZATION);
+
 const eventsModel = new Points();
+const destinationsModel = new Destinations();
+const offersModel = new Offers();
 
 const tripMainElement = document.querySelector(`.trip-main`);
 const bodyContainerElement = document.querySelector(`main .page-body__container`);
 const tripEventsElement = document.querySelector(`.trip-events`);
 
 const tripInfoController = new TripInfoController(tripMainElement, eventsModel);
-const tripEventsController = new TripEventsController(tripEventsElement, eventsModel);
+const tripEventsController = new TripEventsController(tripEventsElement, eventsModel, destinationsModel, offersModel, api);
 const statsComponent = new StatsComponent(eventsModel);
 
 tripEventsController.setAddedNewEventHandler(tripInfoController.enableButtonNewEvent);
@@ -36,10 +41,18 @@ tripInfoController.setOnClickMenuItem((menuItem) => {
   }
 });
 
-api.getEvents()
-  .then((events) => {
-    eventsModel.setEvents(events);
-    render(bodyContainerElement, statsComponent);
-    statsComponent.hide();
-    tripEventsController.render();
-  });
+api.getOffers()
+.then((offers) => offersModel.setOffers(offers))
+.then(() => api.getDestinations())
+.then((destinations) => destinationsModel.setDestinations(destinations))
+.then(() => api.getEvents())
+.then((events) => {
+  eventsModel.setEvents(events);
+  render(bodyContainerElement, statsComponent);
+  statsComponent.hide();
+  tripEventsController.render();
+})
+.catch(() => {
+  // renderError();
+  return [];
+});
